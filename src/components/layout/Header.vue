6e002ad328a8df22c5828a75760f434fb1f97287
Kary <karina@arteko.mx>
2018-02-19 11:07:05 -0600

    COMPONENTES INMEDIATOS

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+			
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
