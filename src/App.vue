c022c18d71ae957d97b3cb813058473bbcc22280
Kary <karina@arteko.mx>
2018-02-19 11:17:44 -0600

    ADD NAVBAR COMPONENT

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div class="app">
+		<app-header></app-header>
+		<div class="container">
+			<router-view></router-view>
+			hi	
+		</div>
+	</div>
 </template>
 
 <script>
+import Header from './components/layout/Header.vue'
 export default {
-  name: 'App'
+	components: {
+		appHeader: Header
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
