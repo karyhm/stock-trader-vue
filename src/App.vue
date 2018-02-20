11d6ff2f026783e557ad433ddc8fcb72d58bd71f
Kary <karina@arteko.mx>
2018-02-20 12:54:36 -0600

    INIZIALIZA LOS STOCKS

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
 		<app-header></app-header>
 		<div class="container">
 			<router-view></router-view>
-			hi	
 		</div>
 	</div>
 </template>
 
 <script>
 import Header from './components/layout/Header.vue'
+
 export default {
 	components: {
 		appHeader: Header
+	},
+	created () {
+		this.$store.dispatch('initStocks')
 	}
 }
 </script>
